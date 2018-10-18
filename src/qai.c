ec212f800a70e1312dba1242196f96e10b761628
Shek Muktar <im.skm07@gmail.com>
2018-10-19 01:22:44 +0530

    FEATURE: Now question answer is written to a storage

--- src/qai.c
+++ src/qai.c
@@ -1,26 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <regex.h>
+#include <unistd.h>
 
 #include "qai.h"
 #include "common.h"
 #include "headers/colors.h"
 
-static unsigned int nque, nans, nunk;
+#define STORAGE "storage.qa"
 
-void qai_start()
+static unsigned int nque, nans, nunk;
+static FILE *qa_file;
+static char current_file[64] = STORAGE;
+static int is_running = 0;
+static int file_exist(char *filename)
 {
-	nque = 0, nans = 0, nunk = 0;
-	fprintf(stderr, ANSI_BOLD ANSI_COLOR_YELLOW"QAI Started\n"ANSI_RESET);
+	if (access(filename, R_OK) == 0)
+		return 1;
+
+	return 0;
 }
 
-void qai_stop()
+static int file_writable(char *filename)
 {
-	fprintf(stderr, ANSI_BOLD ANSI_COLOR_YELLOW"\n\nQAI Stopping\n"ANSI_RESET);
-	fprintf(stderr, "    Questions: %3d\n", nque);
-	fprintf(stderr, "    Answers  : %3d\n", nans);
-	fprintf(stderr, "    Unknowns : %3d\n", nunk);
-	fprintf(stderr, ANSI_BOLD ANSI_COLOR_YELLOW"QAI Stoped\n"ANSI_RESET);
+	if (access(filename, W_OK) == 0)
+		return 1;
+
+	return 0;
+}
+
+static void write_line(FILE *file, char *line)
+{
+	// Do clean up firs
+	// then write
+	fprintf(file, "%s\n", line);
+}
+
+static void clean_line(char *line)
+{
+
+	return;
+}
+
+static FILE *file_init(char *filename)
+{
+
+	return NULL;
 }
 
 void qai_action(char *line, int type, char *var_name, regmatch_t *match)
@@ -34,6 +60,9 @@ void qai_action(char *line, int type, char *var_name, regmatch_t *match)
 				nque++;
 			else if (strcmp(var_name, "answer") == 0)
 				nans++;
+
+			// writing line to storage
+			write_line(qa_file, (char *)&line[match->rm_eo]);
 			break;
 			
 		case KBASE_UNK :
@@ -46,3 +75,44 @@ void qai_action(char *line, int type, char *var_name, regmatch_t *match)
 			break;
 	}
 }
+
+void qai_start()
+{
+	if (file_exist(STORAGE))
+	{
+		if (!file_writable(STORAGE))
+		{
+			sprintf(current_file, "%s-%d", STORAGE, 1);
+			fprintf(stderr, "File %s is not writable\n", STORAGE);
+			fprintf(stderr, "Creating new file %s\n", current_file);
+		}
+	}
+
+	// Opening anc caching file
+	if (is_running == 0)
+	{
+		qa_file = fopen(current_file, "w");
+		if (qa_file == NULL)
+		{
+			fprintf(stderr, "qai_start: failed to create storage file\n");
+			exit(1);
+		}
+		is_running = 1;
+	}
+
+	nque = 0, nans = 0, nunk = 0;
+	fprintf(stderr, ANSI_BOLD ANSI_COLOR_YELLOW"QAI Started\n"ANSI_RESET);
+}
+
+void qai_stop()
+{
+	fprintf(stderr, ANSI_BOLD ANSI_COLOR_YELLOW"\n\nQAI Stopping\n"ANSI_RESET);
+	fprintf(stderr, "    Questions: %3d\n", nque);
+	fprintf(stderr, "    Answers  : %3d\n", nans);
+	fprintf(stderr, "    Unknowns : %3d\n", nunk);
+	fprintf(stderr, ANSI_BOLD ANSI_COLOR_YELLOW"QAI Stoped\n"ANSI_RESET);
+
+	// Do the cleanup
+	is_running = 0;
+}
+
